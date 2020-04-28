b9447c92d143a106ff9377887a91ec8f8c40b599
LynnL4 <lht856@foxmail.com>
2020-04-28 16:16:53 +0800

    rename: rename mpfshell-lite to ardupy-mpfshell

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@
 from mp import version
 from distutils.core import setup
 
-setup(name='mpfshell-lite',
+setup(name='ardupy-mpfshell',
       version=version.FULL,
       description='The lightweight version of the mpfshell is for pure CUI drivers.',
-      author='Stefan Wendler & Juwan',
+      author='Stefan Wendler & Juwan &Hongtai.Liu',
       author_email='junhuanchen@qq.com',
-      url='https://github.com/junhuanchen/mpfshell-lite',
-      download_url='https://codeload.github.com/junhuanchen/mpfshell-lite/zip/master',
+      url='https://github.com/Lynn/mpfshell-lite',
       install_requires=['pyserial', 'websocket_client'],
       packages=['mp'],
-      scripts=['mpfs'],
-      keywords=['micropython', 'shell', 'file transfer', 'development'],
+      scripts=['ardupy-mpfshell'],
+      keywords=['ArduPy', 'micropython', 'shell', 'file transfer', 'development'],
       classifiers=[],
       entry_points={"console_scripts": ["mpfs=mp.mpfshell:main"]},
 )
